f6840506707a018423aed7b65c31572b4f04c354
leobragatti <leonardo.bragatti@gmail.com>
2012-03-29 22:40:45 -0300

    vamos la, colabora

---
+++ pizzaria/pizzaria/entrega/admin.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from django.contrib import admin
+
+from .models import Cliente, Pedido, Entregador, Pizza
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('fone', 'nome', 'endereco')
+    list_display_links = ('fone', 'nome')
+    search_fields = ['fone', 'nome', 'logradouro', 'numero']
+    list_filter = ('logradouro',)
+
+class PizzaAdmin(admin.TabularInline):
+	model = Pizza
+	extra = 1
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('hora_inclusao', 'cliente', 'pronto', 'partiu')
+    list_select_related = True
+    date_hierarchy = 'inclusao'
+    
+    def hora_inclusao(self, obj):
+        return obj.inclusao.strftime('%H:%M')
+        
+    def partiu(self, obj):
+        return bool(obj.pronto and obj.entregador and obj.partida)
+    partiu.boolean = True
+    
+    inlines	= [
+		PizzaAdmin,
+	]
+    
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Entregador)
---
+++ pizzaria/pizzaria/entrega/models.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=128, db_index=True)
+    fone = models.CharField(max_length=16, db_index=True)
+    ramal = models.CharField(max_length=4, blank=True)
+    email = models.EmailField(blank=True)
+    logradouro = models.CharField(max_length=32, db_index=True)
+    numero = models.PositiveIntegerField(u'número')
+    complemento = models.CharField(max_length=32, blank=True)
+    obs = models.TextField(u'observação', blank=True)
+    
+    class Meta:
+        unique_together = ('fone', 'ramal')
+        ordering = ['fone', 'ramal']
+        
+    def __unicode__(self):
+		fone  = self.fone
+		if self.ramal:
+			fone += ' r.' + self.ramal
+		return fone + ' - ' + self.nome
+        
+    def endereco(self):
+        return u'%s, %s' % (self.logradouro, self.numero)
+    endereco.short_description = u'Endereço'
+
+class Pedido(models.Model):
+    inclusao = models.DateTimeField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente)
+    pronto = models.BooleanField(default=False)
+    entregador = models.ForeignKey('Entregador', null=True, blank=True)
+    partida = models.TimeField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.inclusao.strftime('%H:%M') + ' - ' + self.cliente.nome
+    
+class Entregador(models.Model):
+    nome = models.CharField(max_length=64)
+    
+    def __unicode__(self):
+        return self.nome
+        
+    class Meta:
+        verbose_name_plural = u'Entregadores'
+
+SABORES = [
+    ('mussarela', 'Mussarela'),
+    ('portuguesa', 'Portuguesa'),
+    ('calabresa', 'Calabresa'),
+    ('atum', 'Atum')
+]
+
+class Pizza(models.Model):
+    pedido = models.ForeignKey(Pedido)
+    sabor1 = models.CharField(u'sabor 1', max_length=32, choices=SABORES)
+    coberto1 = models.BooleanField(u'cob.')
+    sabor2 = models.CharField(u'sabor 2', max_length=32, choices=SABORES, blank=True)
+    coberto2 = models.BooleanField(u'cob.')
+    obs = models.TextField(u'Observação', blank=True)
+    
+    def __unicode__(self):
+        sabor = self.sabor1
+        if self.coberto1:
+            sabor += ' coberta'
+        if self.sabor2:
+            sabor2 = self.sabor2
+            if self.coberto2:
+                sabor2 += ' coberta'
+            sabor = u'½ %s, ½ %s' % (sabor, sabor2)
+        return sabor
